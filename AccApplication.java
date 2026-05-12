c4b4e5714069bd48d2da3b7755181c8b4efe013d
samir3proprogrammer111 <samir13356892@gmail.com>
2026-05-12 21:06:34 +0530

    Add files via upload

---
+++ AccApplication.java
@@ -0,0 +1,13 @@
+package Banking_Management_System.Acc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccApplication.class, args);
+	}
+
+}
